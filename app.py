4967e509f1db445030959ee7954054129d51d9b9
GirijalaAditya <girijalaaditya@gmail.com>
2021-08-02 11:43:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>SHIVA SHIVA SHIVA</h1>"
+    return "<h1>SHIVA SHIVA SHIVA RAMA RAMA RAMA</h1>"
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=7000, debug=True)
